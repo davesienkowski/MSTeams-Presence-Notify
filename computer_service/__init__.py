008c5d9cad2811332134fa94fe67623391769490
Dave <dave@sienkowski.com>
2026-01-21 15:51:56 -0500

    chore: cleanup

--- computer_service/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-MS Teams Presence Notification Light - Computer Service
-
-This package provides the computer-side service for retrieving MS Teams presence
-status via Microsoft Graph API and serving it to the PyPortal device.
-"""
-
-__version__ = "0.1.0"
-__author__ = "MSTeams Presence Notify Project"
